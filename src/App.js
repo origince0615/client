bff0bb49137a3a2829810a4e38e2294cdc99c978
Eric <nikehero.biz@gmail.com>
2023-02-06 15:48:48 -0500

    submit-commit

--- src/App.js
+++ src/App.js
@@ -3,11 +3,26 @@ import axios from "axios";
 
 import "./App.css";
 
+const SERVER_URL = "http://localhost:3003";
 function App() {
   const [date, setDate] = useState("");
   const [vendor, setVendor] = useState("");
   const [file, setFile] = useState();
   const inputRef = useRef(null);
+
+  const onHandleSubmit = async (event) => {
+    const formData = new FormData();
+    formData.append("date", date);
+    formData.append("vendor", vendor);
+    formData.append("file", file);
+
+    const res = await axios.post(`${SERVER_URL}/api/dtaa`, formData);
+    if (res.data.valid === true) {
+      alert("Upload successfuly!! This is a valid data!");
+    } else {
+      alert("Upload failed! This is not an valid data!");
+    }
+  };
 
   const handleFileChange = useCallback((event) => {
     if (!event.target.files) {
@@ -18,7 +33,7 @@ function App() {
 
   return (
     <div className="App">
-      <form>
+      <form onSubmit={onHandleSubmit}>
         <label>
           Date:
           <input
