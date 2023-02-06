0dc922c9356aa68d362ced28af464730f476fc46
Eric <nikehero.biz@gmail.com>
2023-02-06 15:32:48 -0500

    ui-part

--- src/App.js
+++ src/App.js
@@ -1,23 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useCallback, useRef } from "react";
+import axios from "axios";
+
+import "./App.css";
 
 function App() {
+  const [date, setDate] = useState("");
+  const [vendor, setVendor] = useState("");
+  const [file, setFile] = useState();
+  const inputRef = useRef(null);
+
+  const handleFileChange = useCallback((event) => {
+    if (!event.target.files) {
+      return;
+    }
+    setFile(event.target.files[0]);
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form>
+        <label>
+          Date:
+          <input
+            type="date"
+            value={date}
+            onChange={(e) => {
+              setDate(e.target.value);
+            }}
+          />
+        </label>
+        <label>
+          Vendor name:
+          <input
+            type="text"
+            value={vendor}
+            onChange={(e) => {
+              setVendor(e.target.value);
+            }}
+          />
+        </label>
+        <label>
+          File:
+          <input type="file" ref={inputRef} onChange={handleFileChange} />
+        </label>
+        <input type="submit" className="submitButton" />
+      </form>
     </div>
   );
 }
